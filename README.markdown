f9c338693cd256dda832d448eb1cfaa9637d328f
clemensbasler <clemens-basler@t-online.de>
2016-07-20 15:20:00 +0200

    adding tests

--- README.markdown
+++ README.markdown
@@ -1,1 +1,2 @@
 Getting webpack work with Gulp :) Run npm start to start a local server!
+starting: npm start
---
+++ test/test.test.js
@@ -0,0 +1,20 @@
+/*
+ * Unit tests for lib/calculator.js
+ */
+
+describe('Test', function() {
+
+  // inject the HTML fixture for the tests
+  beforeEach(function() {});
+
+  // remove the html fixture from the DOM
+  afterEach(function() {});
+
+  // call the init function of calculator to register DOM elements
+  beforeEach(function() {});
+
+  it('should 3 to be 3', function() {
+    expect('3').toBe('3');
+  });
+
+});
